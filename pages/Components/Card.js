9f67e9d00a3b18c0abba53b96819bb0be8e8be18
danieljliu <danieljliu3@gmail.com>
2022-07-15 16:03:30 -0400

    switched <Image> to <img>

--- pages/Components/Card.js
+++ pages/Components/Card.js
@@ -4,7 +4,7 @@ export default function Card({image, title, text, code_link}) {
     return (
         <div className="flex justify-center">
             <div className="flex flex-col md:flex-row md:max-w-xl  rounded-lg bg-gray-100 dark:bg-gray-400 shadow-lg">
-                <Image
+                <img
                     className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg outline-1 outline-gray-800"
                     src={image}/>
 
